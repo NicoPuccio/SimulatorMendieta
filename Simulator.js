022701e193465d33ee8735cfdda31ec044858b61
NicoPuccio <nico.puccio@hotmail.com>
2020-02-06 17:28:38 -0300

    Created new instructions in the execute() function. write_pin, read_pin, jmp, jz, jnz, jne, jlt, jlte, jgt and jgte.

--- Simulator.js
+++ Simulator.js
@@ -3,9 +3,9 @@ function ctorSimulator()
   let simulator={
      pins: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      stack: [],
-     pushInstruction: pushInstruction,
      next: next,
      execute: execute,
+     loadInstructions: loadInstructions,
      instructions:[],
      pc:0
   };
@@ -19,10 +19,18 @@ function ctorSimulator()
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+  function loadInstructions(newIns)
+  {
+    simulator.pc = 0;
+    simulator.stack = [];
+    simulator.instructions = newIns;
+  }
+
   function writeConsole(pin)
   {
     console.log(simulator.pins[pin]);
   }
+
   function changeRandomPinValue()
   {
     let r = getRandomInt(0,10);
@@ -30,22 +38,23 @@ function ctorSimulator()
     console.log("Pin" + r + " = " + simulator.pins[r])
   }
 
-  function pushInstruction(keyWord, pin)
-  {
-    simulator.stack.push(pin);
-    simulator.stack.push(keyWord);
-  }
-
   function next()
   {
+    simulator.pc = simulator.pc % simulator.instructions.length;
     var result = simulator.instructions[(simulator.pc++)];
-    simulator.pc=simulator.pc%simulator.instructions.length;
+    simulator.pc = simulator.pc % simulator.instructions.length;
     return result;
   }
 
   function execute()
   {
+
     let instruction = simulator.next();
+    if(instruction == undefined)
+    {
+      throw "undefined found as instruction" ;
+      simulator.pc=0;
+    }
     switch (instruction.op_code) {
       case 'turn_on':
         simulator.pins[instruction.arg] = 1;
@@ -53,7 +62,101 @@ function ctorSimulator()
         case 'turn_off':
             simulator.pins[instruction.arg] = 0;
         break;
+        case'write_pin':
+        simulator.pins[instruction.arg] = simulator.stack.pop();
+        break;
+        case'read_pin':
+        if (instruction.arg < 0 ) {
+          instruction.arg = 0;
+        }
+        else if (instruction.arg > 1) {
+          instruction.arg = 1;
+        }
+        simulator.stack.push(instruction.arg);
+        break;
+        case'read_global':
+        throw "TO DO";
+        break;
+        case'write_global':
+          throw "TO DO";
+        break;
+        case'script_start':
+          throw "TO DO";
+        break;
+        case'script_resume':
+          throw "TO DO";
+        break;
+        case'script_stop':
+          throw "TO DO";
+        break;
+        case'script_pause':
+          throw "TO DO";
+        break;
+        case'jmp':
+          simulator.pc += instruction.arg;
+        break;
+        case'jz':
+          if (simulator.stack.pop() == 0) {
+              simulator.pc += instruction.arg;
+          }
+        break;
+        case'jnz':
+          if (simulator.stack.pop() != 0) {
+            simulator.pc += instruction.arg;
+          }
+        break;
+        case 'jne':
+        {
+        let a = simulator.stack.pop();
+        let b = simulator.stack.pop();
+        if (a != b) {
+          simulator.pc += instruction.arg;
+          }
+        }
+        break;
+        case 'jlt':
+        {
+        let a = simulator.stack.pop();
+        let b = simulator.stack.pop();
+        if (a < b) {
+          simulator.pc += instruction.arg;
+          }
+        }
+        break;
+        case 'jlte':
+        {
+        let a = simulator.stack.pop();
+        let b = simulator.stack.pop();
+        if (a <= b) {
+          simulator.pc += instruction.arg;
+        }
+        }
+        break;
+        case 'jgt':
+        {
+        let a = simulator.stack.pop();
+        let b = simulator.stack.pop();
+        if (a > b) {
+          simulator.pc += instruction.arg;
+        }
+        }
+        break;
+        case 'jgte':
+        {
+        let a = simulator.stack.pop();
+        let b = simulator.stack.pop();
+        if (a >= b) {
+          simulator.pc += instruction.arg;
+        }
+        }
+        break;
+        case 'read_local':
+        {
+          throw "TO DO";
+        }
+        break;
       default:
+    throw "Missing instruction "+instruction.op_code;
 
     }
   }

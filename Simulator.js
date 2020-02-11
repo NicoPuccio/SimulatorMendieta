c0a7602f08ff9cd4b104c6eb363a189d12a91bfa
RomiBarbosa <romy_jul@hotmail.com>
2020-02-11 16:12:11 -0300

    Added labels to pins to recognize them

--- Simulator.js
+++ Simulator.js
@@ -26,16 +26,26 @@ function ctorSimulator()
 {
 	target.innerHTML="";
 	let x= 0;
+  let index = 0;
 	for( let i=0;i<simulator.pins.length;i++)
 	{
 		x+=radius*2;
 		let c =  document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    let t = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 		c.setAttribute("fill", "blue");
 		c.setAttribute("cx", x);
 		c.setAttribute("cy", radius);
 		c.setAttribute("r", radius);
+    t.setAttribute("x", x - 15);
+    t.setAttribute("y",(radius + 40));
+    t.setAttribute( "font-size", "25");
+    t.setAttribute( "width", "50");
+    t.setAttribute("height", "50");
+    t.textContent = "D" + index++;
+    t.setAttribute('fill', '#000');
+    t.setAttribute("viewBox", "0 0 1000 300");
 		target.appendChild(c);
-
+    target.appendChild(t);
 		setInterval(()=>{
 			if(sim.pins[i]>0.5)
 			{
